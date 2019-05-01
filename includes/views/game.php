6c87f260dbe8831979916a9c1d334ab89897747b
AK4738 <44058455+AK4738@users.noreply.github.com>
2019-05-01 13:51:37 +0200

    Add files via upload

--- includes/views/game.php
+++ includes/views/game.php
@@ -1,8 +1,146 @@
 <?php
 // Verbindung
 session_start();
+//Antwort möglichkeiten
+//var_dump($_POST);
+/*if(isset($_POST['action']) == 'answer') {
+    $fid = $_POST['fid'];
+    $answer = '';
+    if(isset($_POST['a3'])) {
+        $answer = 'a3';
+    }
+    if(isset($_POST['a2'])) {
+        $answer = 'a2';
+    }
+    if(isset($_POST['a4'])) {
+        $answer = 'a4';
+    }
+    if(isset($_POST['a1'])) {
+        $answer = 'a1';
+    }
+}*/
+
+//spielstart
+if(isset($_SESSION['gameStatus']) && $_SESSION['gameStatus'] == 'gameOver' || !isset($_SESSION['gameStatus'])) {
+    $_SESSION['gameStatus'] = 'running';
+    $_SESSION['questionsAsked'] = array();
+}
+//Schwierigkeit für seite
+if($_SESSION['gameStatus'] == 'running') {
+    $sid = $_GET['sid'];
+}
+//frage besorgen
+$db= new mysqli('localhost', 'root', '', 'quitz');
+//check connection
+if(!$db){
+    echo 'connection Failed' . mysqli_connect_errno();
+}
+//Frage herausziehen
+$questionsAskedString = implode(',', unserialize($_SESSION['questionsAsked']));
+$optionalExclude = '';
+if($questionsAskedString != '') {
+    $optionalExclude = 'AND FID NOT IN ('.$questionsAskedString.')';
+}
+$sql="SELECT * FROM tquestions WHERE SID=".intval($sid).' '.$optionalExclude.' LIMIT 1';
+$fragenresult = $db->query($sql);
+if($fragenresult == false) {
+    echo $db->error;
+}
+else{
+    $fragenObject = $fragenresult->fetch_object();
+}
+
+//pruefen $fragenObject == null?
+if($fragenObject == null)
+{
+    $_SESSION['questionsAsked'] = '';
+
+    //speichern score in der Datenbank
+    //Redirect auf eine andere SEite?
+}
+//$fragenresult == null?
+//var_dump($fragenresult);
+//close
+
+//richtige Antwort auswählen
 
 
+
+if(isset($_POST['action']) == 'answer') {
+
+    $rant = '';
+    if(isset($_POST['a3'])) {
+        if ($fragenObject->ra == $fragenObject->a3)
+        {
+            echo 'richtig';
+        }
+        else{
+            echo 'falsch';
+
+        }
+    }
+    if(isset($_POST['a2'])) {
+        if ($fragenObject->ra == $fragenObject->a2)
+        {
+            echo 'richtig';
+        }
+        else{
+            echo 'falsch';
+
+        }
+    }
+    if(isset($_POST['a4'])) {
+        if ($fragenObject->ra == $fragenObject->a4)
+        {
+            echo 'richtig';
+
+
+        }
+        else
+        {
+            echo 'falsch';
+
+        }
+    }
+    if(isset($_POST['a1'])) {
+        if ($fragenObject->ra == $fragenObject->a1)
+        {
+            echo 'richtig';
+
+        }
+        else{
+            echo 'falsch';
+
+
+        }
+    }
+
+    $alreadyAnsweredQuestions = unserialize($_SESSION['questionsAsked']);
+
+    $alreadyAnsweredQuestions[] = $_POST['fid'];
+
+    $_SESSION['questionsAsked'] = serialize($alreadyAnsweredQuestions);
+}
+
+/*if(isset($_POST['los']) == 'jok') {
+    if(isset($_POST['j1'])) {
+        echo 'amk';
+
+    }
+    if(isset($_POST['j2'])) {
+        echo 'amkasdf';
+    }
+    if(isset($_POST['j3'])) {
+        echo 'amasdk';
+    }
+}
+*/
+
+
+
+
+
+$db->close();
 ?>
 
 
@@ -33,6 +171,23 @@ session_start();
 
 
 </div>
+<form method="post">
+    <div>
+        <button type="submit" class="btn btn-primary" class="Joker1" name="j1">50/50</button>
+
+    </div>
+
+    <div>
+        <button type="submit" class="btn btn-primary" class="Joker2" name="j2">50/50</button>
+
+    </div>
+
+    <div>
+        <button type="submit" class="btn btn-primary" class="Joker3" name="j3">50/50</button>
+
+    </div>
+    <input type="hidden" name="los" value="jok">
+</form>
 
 <div class="Liste">
     <ul class="list-group">
